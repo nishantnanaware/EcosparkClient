bbe63cb5f90c1bf9f508bbba4d0f2943adcffebc
pavanmali948 <pavanmali948@gmail.com>
2026-04-21 18:41:12 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
